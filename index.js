4d1a30729e1c575cb9dc2895a85a82ef8e3b72dd
Stan Chan <stanchanpsu@outlook.com>
2018-01-17 04:06:34 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const SINGLE = 'SINGLE';
+const DOUBLE = 'DOUBLE';
+const LONG = 'LONG';
+
+exports.handler = (event, context, callback) => {
+
+    var clickType = event.clickType;
+    console.log('Event:', event);
+
+};
